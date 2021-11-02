359644d31b6699da4f7525640893fbbedc53bfb1
koti meesala <kotislr75@gmail.com>
2021-11-02 07:41:25 -0500

    history component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -6,36 +6,54 @@ import {
   Route,
   NavLink
 } from "react-router-dom";
-import Home from './components/Home';
+import Conversion from './components/Conversion';
 import Operations from './components/Operations';
+import History from './components/History';
+import Home from './components/Home';
 
 
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   useEffect(() => {
     document.title = "IEEE 754 Converter"
   }, [])
 
+  const handleEnter = (event) => {
+    if(event.key === "Enter") {
+      setLoggedIn(true);
+    }
+  }
+
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <div className="menu">
-                <NavLink exact activeClassName="active" to="/">Conversion</NavLink>
-                <NavLink activeClassName="active" to="/about">Operations</NavLink>
-          </div>
-          <Switch>
-            <Route path="/about">
-              <Operations />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </header>
+        {loggedIn ?
+          <header className="App-header">
+            <div className="menu">
+              {/* <NavLink exact activeClassName="active" to="/">Home</NavLink> */}
+              <NavLink activeClassName="active" to="/conversion">Conversion</NavLink>
+              <NavLink activeClassName="active" to="/operations">Operations</NavLink>
+              <NavLink activeClassName="active" to="/history">History</NavLink>
+            </div>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/operations">
+                <Operations />
+              </Route>
+              <Route path="/conversion">
+                <Conversion />
+              </Route>
+              <Route path="/history">
+                <History />
+              </Route>
+            </Switch>
+          </header>
+          : <Home handleEnter = {handleEnter}/>}
       </div>
-
-
     </Router>
   );
 }
---
+++ src/components/History.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+
+
+
+function History() {
+
+    return ( <
+        div className = "wrapper" >
+        History <
+        /div>
+    )
+};
+
+export default History;
